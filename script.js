e7c60011d4dc7362cb388ab9188d9b58e46956f5
Worldsalt3 <riliwanhazzan@gmail.com>
2020-10-18 14:28:13 +0100

    add close icon and functionality

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ let menu = document.getElementById('menu');
 
 let navBarToggle = document.getElementById('navbar-toggle');
 
+let bars = document.getElementById('bars');
+
+let close = document.getElementById('close');
+
+let logo = document.getElementById('logo');
+
 navBarToggle.addEventListener('click', () => {
+    navBarToggle.classList.add('hide-navbar-toggle');
     menu.classList.toggle('active');
+    close.classList.toggle('show-close')
+})
+
+close.addEventListener('click', () => {
+  navBarToggle.classList.remove('hide-navbar-toggle')
+  menu.classList.toggle('active')
+  close.classList.toggle('show-close');
 })
