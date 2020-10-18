66e5ce35fe4ebe318cb1986a8ebad8c5d764fe0f
Worldsalt3 <riliwanhazzan@gmail.com>
2020-10-18 17:12:57 +0100

    improve bars and close functionality

--- script.js
+++ script.js
@@ -11,11 +11,13 @@ let logo = document.getElementById('logo');
 navBarToggle.addEventListener('click', () => {
     navBarToggle.classList.add('hide-navbar-toggle');
     menu.classList.toggle('active');
-    close.classList.toggle('show-close')
+    close.classList.toggle('show-close');
+    logo.classList.add('logo-inactive');
 })
 
 close.addEventListener('click', () => {
   navBarToggle.classList.remove('hide-navbar-toggle')
   menu.classList.toggle('active')
   close.classList.toggle('show-close');
+  logo.classList.remove('logo-inactive')
 })
